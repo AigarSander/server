2a6a355fa489275bf99bc0010734f1397a24f0d3
Aigar Sander <aigar.sander@khk.ee>
2017-12-06 02:48:56 +0200

    Created an Application class for Silex

---
+++ projekt1/src/Application.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace BookingApp;
+
+use Silex\Application as SilexApplication;
+use Silex\Provider\DoctrineServiceProvider;
+use Silex\Provider\TwigServiceProvider;
+
+class Application extends SilexApplication {
+
+	public function __construct(array $values = []) {
+		parent::__construct($values);
+
+		$this['debug'] = true;
+
+		$this->register(new TwigServiceProvider(), [
+			'twig.path' => __DIR__.'/../views',
+		]);
+
+		$this->register(new DoctrineServiceProvider(), [
+			'db.options' => [
+				'driver' => 'pdo_sqlite',
+				'path' => __DIR__.'/../database/app.db',
+			],
+		]);
+
+		if (!$this['db']->getSchemaManager()->tableExist('bookings')) {
+			$this['db']->executeQuery(
+				"CREATE TABLE bookings (
+				id INT UNSIGNEED AUTO_INCREMENT PRIMARY KEY,
+				firstName VARCHAR(40) NOT NULL,
+				lastName VARCHAR(40) NOT NULL,
+				phone VARCHAR(10) NOT NULL,
+				email VARCHAR(20) DEFAULT NULL,
+				birthday DATE NOT NULL,
+				startDate DATE NOT NULL,
+				endDate DATE NOT NULL,
+				arrivalTime TIME DEFAULT NULL,
+				additionalInformation TEXT,
+				nrOfPeople INT NOT NULL,
+				payingMethod VARCHAR(10) NOT NULL
+				);"
+			);
+		}
+
+		$this->get('/bookings/create', function () {
+			return $this['twig']->render('base.html.twig');
+		});
+	
+	}
+
+}
